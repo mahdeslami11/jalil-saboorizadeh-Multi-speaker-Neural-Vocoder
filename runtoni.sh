fbad7a610250843842bdc46bf9fe56ca965f14b3
Barbany <oriol.barbany@alu-etsetb.upc.edu>
2018-03-13 18:47:19 +0100

    rm run.sh

--- runtoni.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DB=73
-name=${DB}ulaw
-
-source ~/env/bin/activate
-date
-srun -pveu -J samplernn -c 2 --gres=gpu:1 --mem 102400 python3 -u train.py --ulaw True --exp test --frame_sizes 16 4 --n_rnn 2 --dataset 73
