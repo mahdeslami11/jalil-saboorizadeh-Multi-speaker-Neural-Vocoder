9b848d94deb511133f9fefebe3075b157c9448c5
Barbany <36383612+Barbany@users.noreply.github.com>
2018-03-14 18:03:12 +0100

    "run_nou"

--- run_ahocoder_job_array.sh
+++ run_ahocoder_job_array.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p log_dir # directory for log files
 #sbatch -pveu --array 1-1453%50 ahocoder_instance.sh
-MSG="hola toni" sbatch -pveu --array 6-2703%50 ahocoder_instance.sh
-
+MSG="hola toni" sbatch -pveu --array 1-1921%50 ahocoder_instance.sh
