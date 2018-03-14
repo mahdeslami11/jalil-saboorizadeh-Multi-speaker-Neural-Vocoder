520bc9da63a6da033586761c2d8df3961ad0b6f4
Barbany <36383612+Barbany@users.noreply.github.com>
2018-03-14 17:32:54 +0100

    List update

--- tcstar/dataset.py
+++ tcstar/dataset.py
@@ -18,17 +18,20 @@ for file in files:
         minTime = time
 
 # Randomly take audio files from each of the speakers until minimum duration is reached
+open('wav.list', 'w').close()           # Clear file before appending
+generalFile = open(path + 'wav.list', "a")
 partitions = {'train': 0.6, 'validation': 0.2, 'test': 0.2}
 for part in partitions:
     open('wav_' + part + '.list', 'w').close()           # Clear file before appending
-    outFile = open(path + 'wav.list', "a")
+    outFile = open(path + 'wav_' + part + '.list', "a")
     minTimePart = minTime*partitions[part]
     for file in files:
         time = 0
         table = pd.read_csv(path + file)
         IDList = table.index.tolist()
-        while time < minTimePart:
+        while time < minTimePart and IDList:
             ID = random.choice(IDList)
             outFile.write(table['File Name'][ID][3:5] + '/' + table['File Name'][ID] + '\n')
+            generalFile.write(table['File Name'][ID][3:5] + '/' + table['File Name'][ID] + '\n')
             time += table['Duration (s)'][ID]
             IDList.remove(ID)
