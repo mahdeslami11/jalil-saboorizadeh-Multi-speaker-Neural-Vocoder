41e2122c2d17292d04244f200cff1aa4c7318e94
Barbany <oriol.barbany@alu-etsetb.upc.edu>
2018-06-14 10:04:24 +0200

    Update verbosity

--- model.py
+++ model.py
@@ -217,7 +217,7 @@ class FrameLevelRNN(torch.nn.Module):
         # output torch.Size([128, 1024, 512])
         # (=> 64 frames, 16 input samples/frame)
         if verbose:
-            print('Cond has input size:', prev_samples.size(), 'upper conditioning', upper_tier_conditioning is None)
+            print('Prev samples have input size:', prev_samples.size(), 'upper conditioning', upper_tier_conditioning is None)
         input_rnn = self.input_expand(
           prev_samples.permute(0, 2, 1)
         ).permute(0, 2, 1)
