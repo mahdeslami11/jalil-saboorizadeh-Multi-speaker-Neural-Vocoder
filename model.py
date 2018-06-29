93de0a9190948b52802282ae11e9d3c156fcd09e
Barbany <oriol.barbany@alu-etsetb.upc.edu>
2018-06-29 13:58:49 +0200

    More quantization steps

--- model.py
+++ model.py
@@ -520,7 +520,7 @@ class Generator(Runner):
                 prev_samples, upper_tier_conditioning
             ).squeeze(1).exp_().data
             self.distribution2histogram(sample_dist.cpu().numpy(), original_name, writer,
-                                        iteration=i, quantization=10000)
+                                        iteration=i, quantization=1000000)
             sequences[:, i] = sample_dist.multinomial(1).squeeze(1)
 
         torch.backends.cudnn.enabled = cuda_enabled
